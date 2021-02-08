dfaa5bbb686c2548722d6d3a9185415201b5dab5
EmilioSanche <72004952+EmilioSanche@users.noreply.github.com>
2021-02-08 08:55:11 +0100

    Update

--- src/query.js
+++ src/query.js
@@ -30,7 +30,24 @@ db.stationery.aggregate([
     }}
 ])
 
-//
+//Agrupamos por los objetos los productos vendidos en junio y lo agrupamos por el propio
+//producto y el almacén en el que se encuentra.
 db.stationery.aggregate([
-    
+    {$group:{
+        _id:"$client_name",
+        product:{ "$first": "$product_name" },
+        date:{$first:"$purchase_date"},
+        total_prize:{$sum: {$multiply: ["$unitary_prize","$stock"]}},
+        warehouse:{$first:"$warehouse"},
+    }},
+    {$match:
+        {$expr:{$eq:[{$month:"$date"},6]}}
+    },
+    {$group:{
+        _id:"$product",
+        location:{$first:"$warehouse"}
+    }}
 ])
+//Debido a que algunos valores del _id del primer group se repiten y no pueden ser sumados
+//(como es la fecha) debemos usar un first para tomar solo el primer valor que aparezca al
+//agruparlos por cliente.
